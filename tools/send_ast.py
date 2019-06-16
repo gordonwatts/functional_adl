2495d06d08eeceb1ba70661f61b3ed709eb3bcf7
Gordon Watts <gwatts@uw.edu>
2019-06-16 16:37:01 -0700

    Updated to use the rabbitmq RPC pattern

--- tools/send_ast.py
+++ tools/send_ast.py
@@ -1,17 +1,52 @@
-# Send an ast to a requested queue name on a rabbit server.
+# Send an ast to a requested queue name on a rabbit server using the RPC call pattern
 import sys
 from write_ast import generate_ast
 import pika
 import pickle
+import uuid
+
+done = False
+corr_id = None
+
+def on_response (ch, method, props, body):
+    'We get the info back - print it out'
+    global corr_id
+    global done
+    if corr_id == props.correlation_id:
+        done = True
+        print (body)
 
 def send_ast_msg (ast_number:int, rabbit_address:str, queue_name:str):
     a = generate_ast(ast_number)
 
+    # Open connection to Rabbit, and declare the main queue we will be sending to.
     connection = pika.BlockingConnection(pika.ConnectionParameters(host=rabbit_address))
     channel = connection.channel()
     channel.queue_declare(queue=queue_name)
 
-    channel.basic_publish(exchange='', routing_key=queue_name, body=pickle.dumps(a))
+    # Declare the call-back queue (an anonymous queue)
+    result = channel.queue_declare(queue='', exclusive=True)
+    callback_queue = result.method.queue
+
+    channel.basic_consume(queue=callback_queue, on_message_callback=on_response, auto_ack=True)
+
+    # Now, send the message
+    global corr_id
+    corr_id = str(uuid.uuid4())
+    channel.basic_publish(exchange='',
+        routing_key=queue_name,
+        properties=pika.BasicProperties(
+            reply_to=callback_queue,
+            correlation_id=corr_id
+        ),
+        body=pickle.dumps(a)
+    )
+
+    # Wait for a response
+    global done
+    while not done:
+        channel.connection.process_data_events()
+
     channel.close()
 
 if __name__ == "__main__":
