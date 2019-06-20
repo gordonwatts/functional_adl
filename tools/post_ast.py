9c9997a81d2776e2271812d1a6b4f1f4f98cbee7
Gordon Watts <gwatts@uw.edu>
2019-06-20 00:19:58 -0700

    Make post code "smarter"
    Now will hang around and deliver reports

--- tools/post_ast.py
+++ tools/post_ast.py
@@ -5,16 +5,30 @@ import os
 import pickle
 import requests
 import json
+import time
 
 def send_ast_msg (ast_number:int, base_url:str):
     a = generate_ast(ast_number)
 
     # Build the URL and send it.
     d = pickle.dumps(a)
-    r = requests.post(f'{base_url}/query', headers={"content-type": "application/octet-stream"},  data=d)
 
-    dr = json.loads(r.content)
-    print (dr)
+    done = False
+    first = True
+    while not done:
+        if not first:
+            time.sleep(10)
+        first = False
+
+        r = requests.post(f'{base_url}/query', headers={"content-type": "application/octet-stream"},  data=d)
+
+        if r.content.decode("utf-8")[0:2] != '"{':
+            print (f'Error: {r.content}')
+        else:
+            fixed = r.content.decode("utf-8")[1:-1].replace('\\"', '"')
+            dr = json.loads(fixed)
+            done = dr['done']
+    print (dr['files'])
 
 if __name__ == "__main__":
     bad_args = len(sys.argv) != 3
